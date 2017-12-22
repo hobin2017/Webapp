e4938af4cb37d8fecdff2880eb1e0c302e2623c1
hobin2017 <chenhoubin0919@163.com>
2017-12-23 01:43:07 +0800

    Add files via upload

---
+++ tutorial/tutorial2.py
@@ -0,0 +1,16 @@
+"""
+Modern web frameworks use the routing technique to help a user remember application URLs.
+It is useful to access the desired page directly without having to navigate from the home page.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+#@app.route('/hello') # the first way to bind the url with a function.
+def hello_world():
+    return 'Hello world'
+
+
+if __name__ =='__main__':
+    app.add_url_rule('/hello/', 'hello', hello_world) # the second way to bind the url with a function.
+    app.run('172.16.1.71', debug=True)
+
