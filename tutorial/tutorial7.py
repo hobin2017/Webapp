a599c541a22f3ac0e0edbc76a7a5af76df647fe4
hobin2017 <chenhoubin0919@163.com>
2017-12-26 11:22:50 +0800

    Update tutorial7.py

--- tutorial/tutorial7.py
+++ tutorial/tutorial7.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template("hello(tutorial7).html")
+    return render_template("hello(tutorial7).html")
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
 
 
