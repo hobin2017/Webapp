02c2da2d9fb181fc66e885da4138ffd0ef4a04fd
hobin2017 <chenhoubin0919@163.com>
2017-12-27 20:10:25 +0800

    Add files via upload

---
+++ tutorial/tutorial11.py
@@ -0,0 +1,26 @@
+"""
+Each uploaded file is first saved in a temporary location on the server, before it is actually saved to its ultimate location.
+It is recommended to obtain a secure version of the file name by using the secure_filename() function
+"""
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/upload/')
+def upload():
+    return render_template('upload(tutorial11).html')
+
+
+@app.route('/uploader/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file1']  # It belongs to the werkzeug.datastructures.FileStorage class
+        f.save(secure_filename(f.filename))
+        return '''file uploaded successfully
+        '''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
